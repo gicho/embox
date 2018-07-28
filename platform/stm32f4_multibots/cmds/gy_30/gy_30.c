392bb64570c2efa3a43bf65604437232c81d3aaf
Alex Kalmuk <alexkalmuk@gmail.com>
2018-07-28 15:00:10 +0300

    cmds: (stm32f4) Improve GY-30 example

--- platform/stm32f4_multibots/cmds/gy_30/gy_30.c
+++ platform/stm32f4_multibots/cmds/gy_30/gy_30.c
@@ -6,50 +6,82 @@
  * @author  Alex Kalmuk
  */
 
-#include <errno.h>
-#include <string.h>
 #include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
 
-#include <kernel/irq.h>
-#include <embox/unit.h>
-
-#include "stm32f4xx_hal.h"
-#include "stm32f4xx_hal_i2c.h"
-#include "stm32f4_discovery.h"
-
-#include <drivers/i2c/stm32f4/stm32f4_i2c.h>
 #include <libs/gy_30.h>
 
-static void init_leds(void) {
-	BSP_LED_Init(LED3);
-	BSP_LED_Init(LED4);
-	BSP_LED_Init(LED5);
-	BSP_LED_Init(LED6);
-}
-
-static void stm32f4_delay(int i) {
-	while(--i > 0)
-		;
-}
-
-int main(int argc, char *argv[]) {
-	int res;
-	uint16_t level = 0;
+static void print_lx(uint8_t mode) {
+	uint16_t level;
 
 	printf("GY-30 running\n");
 
-	init_leds();
-	res = gy_30_enable();
-	if (res < 0) {
-		return -1;
+	if (mode == BH1750_CONTINUOUS_HIGH_RES_MODE) {
+		gy_30_setup_mode(mode);
 	}
-	BSP_LED_Toggle(LED3);
 
 	while (1) {
+		if (mode == BH1750_ONE_TIME_HIGH_RES_MODE) {
+			gy_30_setup_mode(mode);
+		}
 		level = gy_30_read_light_level();
-		printf("level: %d\n", (level * 5) / 6);
-		stm32f4_delay(5000000);
+		printf("level: %d lx\n", level);
+		sleep(1);
 	}
+}
+
+static void print_usage(void) {
+	printf("Usage: gy_30 [-h] -s <i2c_nr> -m <mode>\n"
+			"Example:\n"
+			"  gy_30 -s 1 -m continuous_hres\n"
+			"  gy_30 -s 1 -m onetime_hres\n");
+}
+
+int main(int argc, char *argv[]) {
+	int opt;
+	int i2c_nr, mode;
+	char mode_str[32];
+
+	while (-1 != (opt = getopt(argc, argv, "hs:m:"))) {
+		printf("\n");
+		switch (opt) {
+		case 'h':
+			print_usage();
+			return 0;
+		case 's':
+			if ((optarg == NULL) || (!sscanf(optarg, "%d", &i2c_nr))) {
+				print_usage();
+				return -EINVAL;
+			}
+			break;
+		case 'm':
+			if ((optarg == NULL) || (!sscanf(optarg, "%s", mode_str))) {
+				print_usage();
+				return -EINVAL;
+			}
+			if (!strcmp(mode_str, "continuous_hres")) {
+				mode = BH1750_CONTINUOUS_HIGH_RES_MODE;
+			} else if (!strcmp(mode_str, "onetime_hres")) {
+				mode = BH1750_ONE_TIME_HIGH_RES_MODE;
+			} else {
+				print_usage();
+				return -EINVAL;
+			}
+			break;
+		default:
+			print_usage();
+			return -EINVAL;
+		}
+	}
+
+	if (gy_30_init(i2c_nr) < 0) {
+		printf("gy_30_init failed\n");;
+		return -1;
+	}
+
+	print_lx(mode);
 
 	return 0;
 }

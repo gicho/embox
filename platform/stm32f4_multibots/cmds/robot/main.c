171a5653c5ac91b3630cff0d84bb2480a363f213
Alex Kalmuk <alexkalmuk@gmail.com>
2018-07-28 16:07:11 +0300

    cmds: (stm32f4) Improve robot command

--- platform/stm32f4_multibots/cmds/robot/main.c
+++ platform/stm32f4_multibots/cmds/robot/main.c
@@ -7,29 +7,15 @@
  */
 
 #include <errno.h>
-#include <string.h>
 #include <stdio.h>
-#include <assert.h>
+#include <unistd.h>
 
 #include <util/log.h>
-#include <embox/unit.h>
 #include <kernel/thread.h>
-#include <kernel/time/ktime.h>
 
-#include "stm32f4_discovery.h"
+#include <libs/nrf24.h>
+#include <libs/gy_30.h>
 #include <libs/stepper_motor.h>
-#include <libs/ir.h>
-
-#define BH1750_CONTINUOUS_HIGH_RES_MODE  0x10
-
-extern void nrf24_enable(void);
-extern uint16_t gy_30_read_light_level(void);
-extern int gy_30_enable(void);
-
-static void stm32_delay(uint32_t delay) {
-	while (--delay > 0)
-		;
-}
 
 static void init_leds() {
 	BSP_LED_Init(LED3);
@@ -40,34 +26,71 @@ static void init_leds() {
 
 static void *light_sensor_loop(void *arg) {
 	uint16_t level;
-	int i;
 
 	while (1) {
-		for (i = 0; i < 10; i++) {
-			level = gy_30_read_light_level();
-			printf("light (lx): %d\n", level);
-			stm32_delay(1000);
-		}
-		ksleep(10000);
+		level = gy_30_read_light_level();
+		printf("light (lx): %d\n", level);
+		sleep(1);
 	}
+
 	return NULL;
 }
 
+static void robot_config_nrf24(uint8_t *tx_addr, uint8_t *rx_addr,
+								uint8_t chan, int payload_len) {
+	nrf24_init();
+	nrf24_config(chan, payload_len);
+
+	nrf24_rx_address(rx_addr);
+	nrf24_tx_address(tx_addr);
+
+	nrf24_powerUpRx();
+}
+
+static void robot_config_motors(struct stepper_motor *motor1,
+								struct stepper_motor *motor2) {
+	motor_init(motor1, GPIO_PIN_0, GPIO_PIN_2, GPIO_PIN_4, GPIO_PIN_6, GPIOD);
+	motor_init(motor2, GPIO_PIN_8, GPIO_PIN_10, GPIO_PIN_12, GPIO_PIN_14, GPIOE);
+	motor_set_speed(motor1, MOTOR_MAX_SPEED);
+	motor_set_speed(motor2, MOTOR_MAX_SPEED);
+}
+
+static int robot_config_gy_30(int i2c_nr, int mode) {
+	if (gy_30_init(i2c_nr) < 0) {
+		log_error("gy_30_init failed\n");
+		return -1;
+	}
+
+	gy_30_setup_mode(mode);
+
+	return 0;
+}
+
 static void main_loop(void) {
-	struct stepper_motor motor1, motor2;
 	struct thread *th;
+	struct stepper_motor motor1, motor2;
+	uint8_t tx_addr[5] = {0xAA,0xBB,0xCC,0xDD,0x01};
+	uint8_t rx_addr[5] = {0xEE,0xFF,0xAA,0xBB,0x02};
+	int const i2c_nr = 1;
 
-	__GPIOD_CLK_ENABLE(); /* for motors */
-	__GPIOE_CLK_ENABLE(); /* for motors */
+	/* TODO Each robot must set it's own rx and tx address */
+	robot_config_nrf24(tx_addr, rx_addr, 96, 8);
+	printf("NRF24L01+ radio module configured\n");
 
-	nrf24_enable();
-	motor_init(&motor1, GPIO_PIN_0, GPIO_PIN_2, GPIO_PIN_4, GPIO_PIN_6, GPIOD);
-	motor_init(&motor2, GPIO_PIN_8, GPIO_PIN_10, GPIO_PIN_12, GPIO_PIN_14, GPIOE);
-	motor_set_speed(&motor1, MOTOR_MAX_SPEED);
-	motor_set_speed(&motor2, MOTOR_MAX_SPEED);
+	robot_config_motors(&motor1, &motor2);
+	printf("Stepper motors configured\n");
+
+	if (robot_config_gy_30(i2c_nr, BH1750_CONTINUOUS_HIGH_RES_MODE) < 0) {
+		printf("GY-30 initialization failed\n");
+		return;
+	}
+	printf("GY-30 configured\n");
+
+#if 0
 	ir_init();
 	ir_enable();
-	gy_30_enable();
+	HAL_GPIO_WritePin(IR_GPIO, IR_LED_PIN, GPIO_PIN_SET);
+#endif
 
 	th = thread_create(THREAD_FLAG_SUSPENDED, light_sensor_loop, NULL);
 	if (!th) {
@@ -82,10 +105,10 @@ static void main_loop(void) {
 		log_error("Failed to detach thread\n");
 		return;
 	}
-	stm32_delay(1000000);
+
 	while (1) {
 		motor_do_steps2(&motor1, &motor2, 2 * MOTOR_STEPS_PER_REVOLUTION, MOTOR_RUN_BACKWARD);
-		stm32_delay(5000000);
+		usleep(500000);
 	}
 }
 
@@ -94,7 +117,6 @@ int main(int argc, char *argv[]) {
 
 	init_leds();
 	BSP_LED_Toggle(LED6);
-	HAL_GPIO_WritePin(IR_GPIO, IR_LED_PIN, GPIO_PIN_SET);
 
 	main_loop();
 
